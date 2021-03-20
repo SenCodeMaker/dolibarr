541af4a9a05d1a46522ea92226f83cb2e4d84c91
SenCodeMaker <benjamin.dansokho@telecomnancy.net>
2021-03-20 19:57:34 +0100

    main document root

--- htdocs/install/inc.php
+++ htdocs/install/inc.php
@@ -85,7 +85,7 @@ error_log(include_once $conffile);
 
 
 // Load conf file if it is already defined
-if (!defined('DONOTLOADCONF') && file_exists($conffile) && filesize($conffile) > 8) // Test on filesize is to ensure that conf file is more that an empty template with just <?php in first line
+if (!defined('DONOTLOADCONF') == true && file_exists($conffile) && filesize($conffile) > 8) // Test on filesize is to ensure that conf file is more that an empty template with just <?php in first line
 {
 	$result = include_once $conffile; // Load conf file
 	if ($result)
